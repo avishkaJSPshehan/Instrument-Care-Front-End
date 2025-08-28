4acbcd837f014f5e0dcde98124dd0c276278ebc8
Avishka Shehan <152954657+avishkaJSPshehan@users.noreply.github.com>
2025-08-28 15:55:42 +0530

    save user_id instead of username in the front end

--- src/Components/auth/Login.jsx
+++ src/Components/auth/Login.jsx
@@ -36,7 +36,7 @@ export default function LoginPage() {
 
         localStorage.setItem("isLoggedIn", "true");     //  mark logged-in
         localStorage.setItem("role", String(result.role));
-        localStorage.setItem("username", String(result.username));
+        localStorage.setItem("user_id", String(result.user_id));
         
         if (result.role === 8) {
           navigate("/user/dashboard");
