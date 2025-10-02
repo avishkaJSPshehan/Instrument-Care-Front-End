bd966e184549d5463096edbcd9fa9565759ba79b
Avishka Shehan <152954657+avishkaJSPshehan@users.noreply.github.com>
2025-10-02 10:50:30 +0530

    Create new component and page for Reset password in the frontend

--- src/Pages/Auth/Email-Entry-Forget-Password.jsx
+++ src/Pages/Auth/Email-Entry-Forget-Password.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from '../../Components/Common/Navbar'
 import EmailEntry_ForgotPassword from '../../Components/auth/Email-Entry-Forget-Password';
 
-export default function Login() {
+export default function EmailEntryForgotPassword() {
   return (
     <>
       <Navbar />
